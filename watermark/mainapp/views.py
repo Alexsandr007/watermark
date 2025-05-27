2858a38b149f40b1ad9444be7e510bc57eaa71e5
Alex_djangp_projects <aleksone_01@mail.ru>
2025-05-27 18:26:36 +0300

    Add image output

--- watermark/mainapp/views.py
+++ watermark/mainapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import ImageUploadForm
 
-
 def home(request):
+    uploaded_image = None  # для передачи в шаблон
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()  # если используете ModelForm
-            # или обработайте файл как угодно
-            return redirect('home')
+            image_instance = form.save()
+            uploaded_image = image_instance.image.url  # путь к загруженному файлу
+            form = ImageUploadForm()  # очистить форму после загрузки
     else:
         form = ImageUploadForm()
-    return render(request, 'mainapp/home.html', {'form': form})
+    return render(request, 'mainapp/home.html', {'form': form, 'uploaded_image': uploaded_image})
