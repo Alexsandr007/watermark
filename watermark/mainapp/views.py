965b5710c8164f666e564dea2ba478dcd276aa79
Alex_djangp_projects <aleksone_01@mail.ru>
2025-05-27 18:07:18 +0300

    Add views.py for image input

--- watermark/mainapp/views.py
+++ watermark/mainapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageUploadForm
 
-# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()  # если используете ModelForm
+            # или обработайте файл как угодно
+            return redirect('home')
+    else:
+        form = ImageUploadForm()
+    return render(request, 'mainapp/home.html', {'form': form})
